f12bb2b244e194aa2616420d3d4f0566c352ba85
Nithya Priya M. <inc3023@TEMSCBE.com>
2025-08-19 16:24:32 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'static',
+  integrations: [
+    playformCompress(),
+  ]
+});
